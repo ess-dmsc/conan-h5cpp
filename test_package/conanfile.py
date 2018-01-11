5e4d6b3699e692c2af26ff45f062491075e8882d
Afonso Mukai <afonso.mukai@esss.se>
2018-01-11 17:49:08 +0100

    Add LD_LIBRARY_PATH def to linux test

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 import os
 
 
@@ -16,7 +16,13 @@ class GraylogloggerTestConan(ConanFile):
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")
         self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy("*.so*", dst="bin", src="lib")
 
     def test(self):
         os.chdir("bin")
+
+        if tools.os_info.is_linux:
+            ld_library_path = os.environ.get("LD_LIBRARY_PATH", "")
+            os.environ["LD_LIBRARY_PATH"] = ld_library_path + ":."
+
         self.run(".%sexample" % os.sep)
