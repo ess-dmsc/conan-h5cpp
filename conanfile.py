513d36c19f3c3f709f54ab36e855bf90e0e0b8f2
Matt Clarke <matt.clarke@esss.se>
2019-03-19 07:39:45 +0100

    Testing build on Win32

--- conanfile.py
+++ conanfile.py
@@ -91,7 +91,7 @@ class H5cppConan(ConanFile):
             # cmake.configure(source_dir="..", build_dir=".")
             self.run("cmake --debug-output %s %s" % ("..", cmake.command_line))
             cmake.build(build_dir=".")
-            cmake.build(build_dir=".",target="install",args=["--","DESTDIR=./install"])
+            # cmake.build(build_dir=".",target="install",args=["--","DESTDIR=./install"])
 
             os.rename(
                 "../LICENSE",
